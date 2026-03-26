9d9168932adac2fbd401d367427332ff847c82d7
riteshrana12-dev <ranaritesh677@gmail.com>
2026-03-26 16:35:53 +0530

    Refactor: adopt ES module import for api.js

--- client/src/services/authService.js
+++ client/src/services/authService.js
@@ -1,4 +1,4 @@
-import api from "./api";
+import api from "./api.js";
 
 export const authService = {
   register: (data) => api.post("/auth/register", data),
--- client/src/services/medicationService.js
+++ client/src/services/medicationService.js
@@ -1,4 +1,4 @@
-import api from "./api";
+import api from "./api.js";
 
 export const medicationService = {
   add: (data) => api.post("/medications", data),
